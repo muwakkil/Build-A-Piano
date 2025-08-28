9898b62bdbd8d509cc231efb14ff1ee8b563e713
muwakkil <muwakkil@vcu.edu>
2025-08-28 19:45:00 -0400

    keyboard added
    i hope thats it

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//Vars 
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['a', 's', 'd', 'f', 'g'];
+
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+
 // Select all keys
 
 const keys = document.querySelectorAll('.key');
@@ -21,8 +30,24 @@ function playNote(key) {
     key.classList.remove('active');
   });
 
-  // If the key is being held down, don't play the note again
+}
+
+//added
+document.addEventListener('keydown', (e) => {
+
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
   if (e.repeat) {
     return;
   }
-}
+
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
